048b011b9514d0412d300ced9ec0aea0906bfdb9
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-23 12:43:13 +0300

    install bootstrap

--- tribune/settings.py
+++ tribune/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'news',
+    'bootstrap5',
     'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
