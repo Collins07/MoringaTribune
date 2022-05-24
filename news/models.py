68c72ad77af5eba15486321d5bb3df430eab5989
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-24 10:42:26 +0300

    query database

--- news/models.py
+++ news/models.py
@@ -5,3 +5,7 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =20)
     last_name = models.CharField(max_length =20)
     email = models.EmailField()
+
+
+    def __str__(self):
+        return self.first_name
